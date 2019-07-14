59bbf68bdb96b509712ae5abea68a681735a8118
Yang <yangpf0810@sjtu.edu.cn>
2019-07-14 22:03:55 +0800

    first commit

---
+++ src/main/java/com/nowcoder/controller/IndexController.java
@@ -0,0 +1,50 @@
+package com.nowcoder.controller;
+
+import com.nowcoder.model.User;
+import com.sun.deploy.net.HttpResponse;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by lenovo on 2019/7/14.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(path={"/","/index"})
+    @ResponseBody
+    public String index(){
+        return "Hello World";
+    }
+
+    @RequestMapping(path={"/profile/{groupId}/{userId}"})
+    @ResponseBody
+    public String profile(@PathVariable("groupId") String groupId,
+                          @PathVariable("userId") int userId,
+                          @RequestParam(value = "type",defaultValue = "1") int type,
+                          @RequestParam(value = "key",defaultValue = "0") String key){
+        return groupId+": "+userId+":"+type+":"+key;
+    }
+
+    @RequestMapping("/vm")
+    public String news(Model model){
+        model.addAttribute("value1","vv1");
+        model.addAttribute("value2","vv2");
+        model.addAttribute("user",new User("Jim"));
+        return "news";
+    }
+
+    @RequestMapping("/request")
+    @ResponseBody
+    public String request(HttpRequest httpRequest,
+                          HttpResponse httpResponse,
+                          HttpSession httpSession){
+        return "略";
+    }
+}
---
+++ src/main/java/com/nowcoder/model/User.java
@@ -0,0 +1,19 @@
+package com.nowcoder.model;
+
+/**
+ * Created by lenovo on 2019/7/14.
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public User(String name){
+        this.name=name;
+    }
+}
